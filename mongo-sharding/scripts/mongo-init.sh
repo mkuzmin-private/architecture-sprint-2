d2c39307fceb82dd7fd676aacdeecd36633c555d
mkuzmin <mkuzmin0@gmail.com>
2024-11-24 15:40:37 +0700

    sprint2: cleanup

--- mongo-sharding/scripts/mongo-init.sh
+++ mongo-sharding/scripts/mongo-init.sh
@@ -10,6 +10,6 @@ sh.addShard( "shard2/shard2:27019");
 sh.enableSharding("somedb");
 sh.shardCollection("somedb.helloDoc", { "name": "hashed" })
 use somedb
-for(var i = 0; i < 1000; i++) db.helloDoc.insert({age:i, name:"ly"+i})
+for(var i = 0; i < 1000; i++) db.helloDoc.insertOne({age:i, name:"ly"+i})
 EOF
 
