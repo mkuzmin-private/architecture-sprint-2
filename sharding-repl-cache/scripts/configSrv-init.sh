c299d3b2243b7a27532d8343843b7611096cfa70
mkuzmin <mkuzmin0@gmail.com>
2024-11-25 14:19:39 +0700

    sprint2: cache final corrections

---
+++ sharding-repl-cache/scripts/configSrv-init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+###
+# Инициализируем конфиг сервера
+###
+
+docker compose exec -T configSrv mongosh --port 27017 <<EOF
+rs.initiate(
+  {
+    _id: "config_server",
+    configsvr: true,
+    members: [
+      { _id: 0, host: "configSrv:27017" }
+    ]
+  }
+);
+EOF
